0be8086ea72837f471310b63437767b14f1c3e01
unknown <abhishekm@globussoft.com>
2015-09-23 19:42:29 +0530

    made major changes in client side

---
+++ app/modules/client/controllers/auth.js
@@ -0,0 +1,45 @@
+'use strict';
+var express = require('express');
+
+/**
+ * connection to maria database
+ */
+var connect = require('../../../models/maria.js');
+var c = new connect.maria();
+
+
+
+exports.ackverify = function(req, res) {
+	var data = [];
+	c.query('select ackcode from client where ackcode="' + req.body.ackcode + '"')
+		.on('result', function(resrow) {
+
+			resrow.on('row', function(row) {
+					data.push(row)
+				})
+				.on('error', function(err) {
+					console.log('Result error: ' + err);
+				})
+				.on('end', function(info) {
+					if (data.lenght != 0) {
+						var q = 'UPDATE client SET emailverified=1,ackcode=null where ackcode="' + req.body.ackcode + '"';
+						c.query(q)
+							.on('result', function(result) {
+								result.on('end', function(info) {
+									res.json({
+										"status": 200
+									});
+								})
+							})
+
+					} else {
+						res.json({
+							"status": 198
+						});
+					}
+
+				});
+
+		})
+}
+
